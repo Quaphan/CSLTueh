f0c68c3fca643aa8e87f50c436a430192a359b14
Quaphan <quaphan843@gmail.com>
2026-05-15 19:52:11 +0700

    Add project files.

---
+++ Session_05/Exercises_4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSLTueh.Session_05
+{
+    internal class Exercises_4
+    {
+        static bool IsPrime(int n)
+        {
+            if (n < 2)
+                return false;
+
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static void Main(string[] args)
+        {
+            int number = 12;
+
+            if (IsPrime(number))
+                Console.WriteLine(number + " is a prime number");
+            else
+                Console.WriteLine(number + " is not a prime number");
+        }
+    }
+}
